d1e0a7382a21cb2e664e3d0759692a0e0b6bb253
sufeng <sufeng@163.com>
2016-12-25 10:54:40 +0800

    add parser and db.

--- src/main/java/com/sf/wxc/schedule/FeedScheduler.java
+++ src/main/java/com/sf/wxc/schedule/FeedScheduler.java
@@ -8,7 +8,7 @@ import com.sf.wxc.repository.db.feeddb.FeedDbRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -17,8 +17,8 @@ import java.util.List;
 /**
  * Created by Su Feng on 2016/12/22.
  */
+@Lazy(false)
 @Component
-@EnableScheduling
 public class FeedScheduler {
     private static Logger logger = LoggerFactory.getLogger(FeedScheduler.class);
 
