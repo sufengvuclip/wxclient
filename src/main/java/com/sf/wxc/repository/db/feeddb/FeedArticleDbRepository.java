c8fdb99251f656adadc085c69ce36d00052c0a47
sufeng <sufeng@163.com>
2016-12-26 00:33:43 +0800

    add parser and db.

--- src/main/java/com/sf/wxc/repository/db/feeddb/FeedArticleDbRepository.java
+++ src/main/java/com/sf/wxc/repository/db/feeddb/FeedArticleDbRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Created by Su Feng on 2016/12/22.
  */
@@ -14,4 +16,7 @@ public interface FeedArticleDbRepository extends JpaRepository<FeedArticle, Inte
 
     @Query("select  CASE  WHEN count(a)> 0 THEN true ELSE false END from FeedArticle a where a.url=:url")
     public boolean existsUrl(@Param("url") String url);
+
+    @Query("select a from FeedArticle a where a.id>:id order by a.id limit :limit")
+    public List<FeedArticle> queryByMinID(@Param("id") Integer id,@Param("limit") Integer limit);
 }
--- src/main/java/com/sf/wxc/schedule/BigDataWayScheduler.java
+++ src/main/java/com/sf/wxc/schedule/BigDataWayScheduler.java
@@ -3,12 +3,17 @@ package com.sf.wxc.schedule;
 import com.sf.wxc.beans.FeedArticle;
 import com.sf.wxc.repository.db.feeddb.FeedArticleDbRepository;
 import com.sf.wxc.service.BigDataWay;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -21,16 +26,41 @@ public class BigDataWayScheduler {
     FeedArticleDbRepository feedArticleDbRepository;
     @Autowired
     BigDataWay bigDataWay;
+    @Autowired
+    Environment env;
 
     @Scheduled(fixedDelay = 60 * 60 * 1000)
     public void postNewArticle() {
-        List<FeedArticle> articleList = feedArticleDbRepository.findAll();
-        if(articleList!=null && articleList.size()>0){
-            for(FeedArticle article:articleList){
-                String nodeId = bigDataWay.postArticle(article);
-                logger.info("added new article, node id {}",nodeId);
+        List<FeedArticle> articleList = null;
+        String lastidFilePath = env.getProperty("drupal.post.last.artile.id.file");
+        File lastidFile = new File(lastidFilePath);
+        int lastId = -1;
+        do {
+            try {
+                lastId = NumberUtils.toInt(FileUtils.readFileToString(lastidFile,"utf-8"),-1);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        }
+            if(lastId==-1) {
+                logger.error("read lastidFile error {}",lastidFilePath);
+                break;
+            }
+            articleList = feedArticleDbRepository.queryByMinID(lastId, 100);
+            if (articleList != null && articleList.size() > 0) {
+                for (FeedArticle article : articleList) {
+                    String nodeId = bigDataWay.postArticle(article);
+                    if(nodeId!=null && nodeId.length()>0) {
+                        try {
+                            FileUtils.writeStringToFile(lastidFile, String.valueOf(article.getId()), "utf-8");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            break;
+                        }
+                    }
+                    logger.info("added new article {}, node id {}", article.getId(), nodeId);
+                }
+            }
+        }while(articleList!=null && articleList.size()>0);
 
     }
 }
