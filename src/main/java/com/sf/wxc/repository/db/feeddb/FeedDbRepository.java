f83e675e2ecc84b11c1c146381d6a2cbb0d5183c
sufeng <sufeng@163.com>
2016-12-29 00:10:24 +0800

    add feed active attribute.

--- src/main/java/com/sf/wxc/repository/db/feeddb/FeedDbRepository.java
+++ src/main/java/com/sf/wxc/repository/db/feeddb/FeedDbRepository.java
@@ -1,14 +1,15 @@
 package com.sf.wxc.repository.db.feeddb;
 
 import com.sf.wxc.beans.Feed;
-import org.hibernate.mapping.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Iterator;
 
 /**
  * Created by Su Feng on 2016/12/22.
  */
 @Repository
 public interface FeedDbRepository extends JpaRepository<Feed, Integer> {
-    public List findByActiveTrue();
+    public Iterator<Feed> findByActiveTrue();
 }
