75e83788a15876003e931b4154e771c5534ff699
sufeng <sufeng@163.com>
2016-12-29 11:26:30 +0800

    test tor

--- src/main/java/com/sf/wxc/util/MyPlainConnectionSocketFactory.java
+++ src/main/java/com/sf/wxc/util/MyPlainConnectionSocketFactory.java
@@ -1,7 +1,6 @@
 package com.sf.wxc.util;
 
-import org.apache.http.HttpHost;
-import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
 import org.apache.http.protocol.HttpContext;
 
 import java.io.IOException;
@@ -12,7 +11,7 @@ import java.net.Socket;
 /**
  * Created by Administrator on 2016-12-29.
  */
-public class MyPlainConnectionSocketFactory implements ConnectionSocketFactory {
+public class MyPlainConnectionSocketFactory extends PlainConnectionSocketFactory {
     public MyPlainConnectionSocketFactory() {
     }
 
@@ -21,26 +20,5 @@ public class MyPlainConnectionSocketFactory implements ConnectionSocketFactory {
         InetSocketAddress socksaddr = (InetSocketAddress) httpContext.getAttribute("socks.address");
         Proxy proxy = new Proxy(Proxy.Type.SOCKS, socksaddr);
         return new Socket(proxy);
-    }
-
-    @Override
-    public Socket connectSocket(int connectTimeout, Socket socket, HttpHost host, InetSocketAddress remoteAddress, InetSocketAddress localAddress, HttpContext context) throws IOException {
-        Socket sock = socket != null?socket:this.createSocket(context);
-        if(localAddress != null) {
-            sock.bind(localAddress);
-        }
-
-        try {
-            sock.connect(remoteAddress, connectTimeout);
-            return sock;
-        } catch (IOException var11) {
-            try {
-                sock.close();
-            } catch (IOException var10) {
-                ;
-            }
-
-            throw var11;
-        }
     }
 }
