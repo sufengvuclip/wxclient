0bfd0179dc69c6d6665d1606c7d35e5d890a084e
sufeng <sufeng@163.com>
2016-12-30 02:26:47 +0800

    test tor

--- src/main/java/com/sf/wxc/util/MyPlainConnectionSocketFactory.java
+++ src/main/java/com/sf/wxc/util/MyPlainConnectionSocketFactory.java
@@ -3,6 +3,8 @@ package com.sf.wxc.util;
 import org.apache.http.HttpHost;
 import org.apache.http.conn.socket.PlainConnectionSocketFactory;
 import org.apache.http.protocol.HttpContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -13,10 +15,11 @@ import java.net.Socket;
  * Created by Administrator on 2016-12-29.
  */
 public class MyPlainConnectionSocketFactory extends PlainConnectionSocketFactory {
-
+    private static Logger logger = LoggerFactory.getLogger(MyPlainConnectionSocketFactory.class);
     @Override
     public Socket createSocket(HttpContext httpContext) throws IOException {
         InetSocketAddress socksaddr = (InetSocketAddress) httpContext.getAttribute("socks.address");
+        logger.info("socksaddr {}",socksaddr==null?"null":socksaddr.getHostString());
         Proxy proxy = new Proxy(Proxy.Type.SOCKS, socksaddr);
         return new Socket(proxy);
     }
