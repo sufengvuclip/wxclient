2dfe460fbbb7e31fdc6c4b963cec7919570f023d
sufeng <sufeng@163.com>
2016-12-29 10:39:12 +0800

    test tor

--- src/main/java/com/sf/wxc/controller/TestController.java
+++ src/main/java/com/sf/wxc/controller/TestController.java
@@ -8,6 +8,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -27,7 +28,9 @@ public class TestController {
     public ResponseEntity<?> namedEntity(@RequestParam(value = "url", required = true) String url) {
         if (StringUtils.trimToNull(url) == null)
             return new ResponseEntity<String>("invalidate parameter!", HttpStatus.FORBIDDEN);
-        CloseableHttpClient client = HttpClientUtil.getHttpClient(null,false);
+
+        JSONObject login = new JSONObject(loginJson);
+        CloseableHttpClient client = HttpClientUtil.getHttpClient(login,false);
         InetSocketAddress socksaddr = new InetSocketAddress("127.0.0.1",
                 9500);
         HttpClientContext context = HttpClientContext.create();
@@ -46,4 +49,28 @@ public class TestController {
         return new ResponseEntity<String>(result, HttpStatus.OK);
 
     }
+
+    static String loginJson = "{\n" +
+            "  \"loginurl\": \"http://www.tuicool.com/login\",\n" +
+            "  \"formdata\":{\n" +
+            "    \"utf8\":\"✓\",\n" +
+            "    \"authenticity_token\":\"5vg/Z5gF6fAL2cgHONRAHjIqI2PG8zxjB5H5AzpwLOk=\",\n" +
+            "    \"email\":\"sufengster@163.com\",\n" +
+            "    \"password\":\"meiyoumima\",\n" +
+            "    \"remember\":\"1\"\n" +
+            "  },\n" +
+            "  \"headers\":{\n" +
+            "    \"Accept\":\"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\",\n" +
+            "    \"Accept-Encoding\":\"gzip, deflate\",\n" +
+            "    \"Accept-Language\":\"zh-CN,zh;q=0.8,en;q=0.6\",\n" +
+            "    \"Cache-Control\":\"max-age=0\",\n" +
+            "    \"Connection\":\"keep-alive\",\n" +
+            "    \"Content-Type\":\"application/x-www-form-urlencoded\",\n" +
+            "    \"Host\":\"www.tuicool.com\",\n" +
+            "    \"Origin\":\"http://www.tuicool.com\",\n" +
+            "    \"Referer\":\"http://www.tuicool.com/login\",\n" +
+            "    \"Upgrade-Insecure-Requests\":\"1\",\n" +
+            "    \"User-Agent\":\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36\"\n" +
+            "  }\n" +
+            "}";
 }
