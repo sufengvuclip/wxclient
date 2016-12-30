dc85bedbced59e3b4e1d1755aad1eb34d22e69b7
sufeng <sufeng@vuclip.cn>
2016-12-30 13:06:18 +0800

    get boolean value from db, convert method.

--- src/main/java/com/sf/wxc/util/ConvertUtils.java
+++ src/main/java/com/sf/wxc/util/ConvertUtils.java
@@ -1,7 +1,6 @@
 package com.sf.wxc.util;
 
 import org.apache.commons.lang3.math.NumberUtils;
-import org.apache.xpath.operations.Bool;
 
 import java.math.BigInteger;
 import java.sql.Date;
@@ -14,8 +13,8 @@ public class ConvertUtils {
         //System.out.println("convert: value "+value+" class "+clazz.getName());
         if(clazz.equals(String.class))
             return String.valueOf(value);
-        if(clazz.equals(Boolean.class) || "boolean".equals(clazz.getName()))
-            return (NumberUtils.toInt(String.valueOf(value),0)>0);
+        if(clazz.equals(Boolean.class) || "Boolean".equals(clazz.getName()))
+            return Boolean.valueOf(NumberUtils.toInt(String.valueOf(value),0)>0);
         if(clazz.equals(Integer.class) || "int".equals(clazz.getName()))
             return NumberUtils.toInt(String.valueOf(value),0);
         if(clazz.equals(Long.class) || "long".equals(clazz.getName()))
