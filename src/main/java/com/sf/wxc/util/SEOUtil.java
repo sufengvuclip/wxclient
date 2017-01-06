7a774609c77e5ca914123ca9abd7dde0b0a67fa7
sufeng <sufeng@vuclip.cn>
2017-01-06 12:30:15 +0800

    post new aticles to baidu for seo

---
+++ src/main/java/com/sf/wxc/util/SEOUtil.java
@@ -0,0 +1,36 @@
+package com.sf.wxc.util;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Su Feng on 2017/1/6.
+ */
+public class SEOUtil {
+    static final String baiduPostUrl = "http://data.zz.baidu.com/urls?site=www.bigdataway.net&token=0aqOWpP8u9cdmP3j";
+    public static boolean postToBaidu(List<String> urls){
+        boolean ret = false;
+        StringBuffer sb = new StringBuffer();
+        if(urls!=null && urls.size()>0){
+            for(String url:urls)
+                sb.append(url).append("\n");
+            try {
+                String response = HttpClientUtil.httpPostRequest(baiduPostUrl,sb.substring(0,sb.length()-1));
+                System.out.println(response);
+                if(response.contains("success"))
+                    ret = true;
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        List<String> urls = new ArrayList<>();
+        urls.add("http://www.bigdataway.net/node/1185");
+        urls.add("http://www.bigdataway.net/node/1186");
+        boolean ret = postToBaidu(urls);
+    }
+}
