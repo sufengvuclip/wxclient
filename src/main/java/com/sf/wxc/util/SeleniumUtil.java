55268d2eeb8173fbc29aa562bb818fd792fd5f15
sufeng <sufeng@vuclip.com>
2018-03-14 13:48:26 -0700

    install external selenium standalone jar.

--- src/main/java/com/sf/wxc/util/SeleniumUtil.java
+++ src/main/java/com/sf/wxc/util/SeleniumUtil.java
@@ -12,7 +12,7 @@ public class SeleniumUtil {
     static{
         //System.setProperty("phantomjs.binary.path","D:\\TDDOWNLOAD\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe");
         //driver = new org.openqa.selenium.phantomjs.PhantomJSDriver();
-        driver = new HtmlUnitDriver(true);
+        driver = new HtmlUnitDriver( BrowserVersion.FIREFOX_45,true);
         //driver.setJavascriptEnabled(true);
 
     }
